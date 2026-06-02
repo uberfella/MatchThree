60c3cdf1ebb9e932773739b44e7cee031db42c60
Nikita Ivanov <53362543+uberfella@users.noreply.github.com>
2026-06-02 23:04:09 +0700

    development plan 1

---
+++ Assets/Scripts/DevelopmentPlan.cs
@@ -0,0 +1,12 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DevelopmentPlan
+{
+    /*
+    CellManager manages cell -> empty, occupied, occupied with what
+    left click empties cell -> cell is Empty
+    cell underneath empty -> cell above goes down one tile
+    new random cells spawn above the grid invisible, when they go down they become visible
+    */
+}
