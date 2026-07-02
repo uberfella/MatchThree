2246a92a03e005402297e817eecc69626693dba4
Nikita Ivanov <53362543+uberfella@users.noreply.github.com>
2026-07-02 22:53:47 +0700

    FollowingYtTutorial 1

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public GameObject tilePrefab;
+    private BackgroundTile[,] allTiles;
+    void Start()
+    {
+        allTiles = new BackgroundTile[width, height];
+        SetUp();
+    }
+
+
+    private void SetUp()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                Vector2 tempPosition = new Vector2(i, j);
+                GameObject backgroundTile = Instantiate(tilePrefab, tempPosition, Quaternion.identity);
+
+
+            }
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Interfaces/IDestroyable.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public interface IDestroyable
-{
-    void Destroy();
-
-}
